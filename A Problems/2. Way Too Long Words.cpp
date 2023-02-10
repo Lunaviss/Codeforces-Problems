508142bdfe263a4049199de25c44c65481399ab9
Lunavis Edwin <123220142+Lunaviss@users.noreply.github.com>
2023-02-10 21:19:07 +0700

    Update 2. Way Too Long Words.cpp

--- A Problems/2. Way Too Long Words.cpp
+++ A Problems/2. Way Too Long Words.cpp
@@ -17,13 +17,14 @@ int main(int argc, char const *argv[])
 	int n;
 	cin >> n;
 	string str[n];
-	for (int i = 0; i < n; ++i)
+	while (n--)
 	{
 		string localStr;
 		cin >> localStr;
 		str[i] = localStr;
 	}
-	for (int i = 0; i < n; ++i) {
+	while (n--) 
+	{
 		solve(str[i]);
 		cout << '\n';
 	}
