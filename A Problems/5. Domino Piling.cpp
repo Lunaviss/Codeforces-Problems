9b66be711a9af17040050379de4bc5a32dc641a1
Lunavis Edwin <123220142+Lunaviss@users.noreply.github.com>
2023-02-14 12:46:57 +0700

    Create 5. Domino Piling.cpp

---
+++ A Problems/5. Domino Piling.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+	int m, n;
+	cin >> m >> n;
+	cout << m*n/2;
+}
+
+int main(int argc, char const *argv[])
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	solve();
+	return 0;
+}
