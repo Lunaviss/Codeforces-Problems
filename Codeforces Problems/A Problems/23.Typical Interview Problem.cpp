f28434f57178cd8b4d42ef5e90606070ec4948e8
Lunavis Edwin <123220142+Lunaviss@users.noreply.github.com>
2023-03-02 21:18:07 +0700

    Create 23.Typical Interview Problem.cpp

---
+++ Codeforces Problems/A Problems/23.Typical Interview Problem.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve() {
+	int k;
+	cin >> k;
+	const string FB = "FBFFBFFBFBFFBFFBFBFFBF";
+	string str;
+	cin >> str;
+	size_t found = FB.find(str);
+	cout << ((found != string::npos) ? "YES\n" : "NO\n");
+}
+ 
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	// freopen("learning.INP", "r", stdin);
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+}
